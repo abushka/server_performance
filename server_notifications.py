713c9a34abf2de0c3ba5dc264f61fe61f66790b3
Abushka <65396568+abushka@users.noreply.github.com>
2023-05-27 15:38:50 +0500

    Update server_notifications.py

--- server_notifications.py
+++ server_notifications.py
@@ -23,12 +23,19 @@ while True:
             process_name = process.name()
             process_cpu_percent = process.cpu_percent()
             process_memory_percent = process.memory_percent()
-            processes.append("{}: CPU {}%, Mem {}%".format(process_name, process_cpu_percent, process_memory_percent))
+            processes.append((process_name, process_cpu_percent, process_memory_percent))
         except (psutil.NoSuchProcess, psutil.AccessDenied, psutil.ZombieProcess):
             pass
 
-    # Формируем сообщение с данными о нагрузке
-    message = "CPU: {}%\nMemory: {}%\nBytes sent: {}\nBytes recv: {}\n\nProcesses:\n{}".format(cpu_percent, memory_percent, network_io_counters.bytes_sent, network_io_counters.bytes_recv, "\n".join(processes))
+    # Сортировка процессов по убыванию использования процессора и оперативной памяти
+    processes = sorted(processes, key=lambda p: (p[1], p[2]), reverse=True)
+
+    # Формируем сообщение с данными о нагрузке и процессах
+    message = "CPU: {}%\nMemory: {}%\nBytes sent: {}\nBytes recv: {}\n\nProcesses:".format(cpu_percent, memory_percent, network_io_counters.bytes_sent, network_io_counters.bytes_recv)
+
+    for process in processes:
+        process_name, process_cpu_percent, process_memory_percent = process
+        message += "\n{}: CPU {}%, Mem {}%".format(process_name, process_cpu_percent, process_memory_percent)
 
     # Отправляем сообщение через API Telegram
     requests.post(URL, data={"chat_id": CHAT_ID, "text": message})
